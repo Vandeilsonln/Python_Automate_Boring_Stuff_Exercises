1a303b3ce0f302e1ed262d653ba837cc694aeea5
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-09-26 18:19:37 -0300

    function docstring

--- Chapter-9_Organizing-Files/selective_copy.py
+++ Chapter-9_Organizing-Files/selective_copy.py
@@ -5,15 +5,26 @@
 import os, shutil
 
 
-def originPath(folderpath, destinyPath, extension):
-    for currentFolder, subfolders, filenames in os.walk(folderpath):
-        print(f'Looking for {extension} files in {currentFolder}')
-        for file in filenames:
-            if file.endswith(extension):
-                print(f'\tFound file: {os.path.basename(file)}.')
-                newFilePath = os.path.join(destinyPath, file) + extension
-                # print(os.path.join(folderpath, file), newFilePath)
-                shutil.copy(os.path.join(currentFolder, file), newFilePath)
-    print('Done.')
+def copy_files(folderPath, destinyPath, extension):
+    """
+    Cope files from 'folderPath' to 'destinyPath' according to 'extension'.
 
-originPath('C:\\delicious', 'C:\\Python_Projetos', '.zip')
+    folderPath (str) - absolute path of the origin folder.
+    destinyPath (str) - absolute path of the destination folder
+    extension (str) - extension that will work as a filter. Only files with this extension will be copied.    
+    """
+    try:
+
+        for currentFolder, subfolders, filenames in os.walk(folderPath):
+            print(f'Looking for {extension} files in {currentFolder}')
+            for file in filenames:
+                if file.endswith(extension):
+                    print(f'\tFound file: {os.path.basename(file)}.')
+                    newFilePath = os.path.join(destinyPath, file) + extension
+                    shutil.copy(os.path.join(currentFolder, file), newFilePath)
+    except:
+        print('Something went wrong. Please, verify the function arguments and try again.')    
+    print('Done.')
+    return None
+
+copy_files('C:\\delicious', 'C:\\Python_Projetos', '.zip')
