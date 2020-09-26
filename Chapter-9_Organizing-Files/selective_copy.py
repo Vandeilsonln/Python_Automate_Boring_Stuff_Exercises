ebe96149e12e22e04e157aaeaae1bf3dd92229c4
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-09-26 12:56:05 -0300

    Create selective_copy.py
    Practice Projects from the book. Page 213.

---
+++ Chapter-9_Organizing-Files/selective_copy.py
@@ -0,0 +1,5 @@
+#! python3
+# selective_copy.py - Once given a folder path, the program will walk through the folder tree
+# and will copy a specific type of file (e.g. .txt or .pdf). They will be copied to a new folder
+
+import os, shutil
