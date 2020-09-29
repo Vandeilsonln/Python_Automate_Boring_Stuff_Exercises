f16751bc3744f1c2de7ee61a34478898c0d1c5a3
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-09-28 23:05:31 -0300

    Update renamepictures.py
    It's is somehow not working with a few .jpg files.

--- Chapter-9_Organizing-Files/renamepictures.py
+++ Chapter-9_Organizing-Files/renamepictures.py
@@ -1,4 +1,10 @@
 #! python3
+# renamepictures.py - The code will go through a directory tree and will rename all the pictures
+# based on it's creation date.
+
+# For now the code will just identify '.jpg' files. Any other formats will remain unchanged, although
+# their filenames will be written in the 'rejected.txt' file. By doing this, you are aware of any
+# other formats you have to deal with.
 
 import os, shutil
 from PIL import Image
@@ -6,31 +12,28 @@ from PIL import Image
 
 os.chdir(r'C:\Users\aline\Pictures\Viagens')
 root = os.getcwd()
-picsPath = os.listdir(root)
-ext = ['.jpg', '.mp4']
 
-for i in range(len(ext)):
-    for folderName, subfolders, filenames in os.walk(root):
-        print(f'Looking for {ext[i]} files in {folderName}')
-        for file in filenames:
+# TODO: Create a list of acceptable extension files and make the code working with all of them.
+ext = '.jpg'
+
+for folderName, subfolders, filenames in os.walk(root):     # we won't be using 'subfolders' here, but it is still needed to unpack the os.walk() function's correctly.
+    print(f'Looking for {ext} files in {folderName}')
+    for file in filenames:      # every actual file that is inside the current folder.
+        print(file)
+        if not file.endswith(ext):
+            print("File's extension rejected")
+            with open('.\\rejected.txt', 'a') as rejected:
+                rejected.write(file)
+                rejected.write('\n')
+            continue
+        else:
             oldName = os.path.abspath(folderName) + '\\' + file
-            myFile, myExt = os.path.splitext(oldName)
-            print(myExt.lower())
-            
-            if not file.endswith(myExt.lower()):
-                print('rejected')
-                with open('.\\rejected.txt', 'a') as rejected:
-                    rejected.write(file)
-                    rejected.write('\n')
-                continue
-            try:
-                with Image.open(oldName) as photo:
-                    info = photo._getexif()
-                    newFileName = root + '\\' + (info[36867].replace(':', '_')) + ext[i]
-                # shutil.move(os.path.abspath(file), newFileName)
-                # print(file)
-                # print(oldName)
-                # print(newFileName)
-                # print('-'*50)
-            except:
-                print('an error ocurred.')
+
+            with Image.open(oldName) as photo:
+                info = photo._getexif()
+                newFileName = root + '\\' + (info[36867].replace(':', '_')) + ext
+            # shutil.move(oldName, newFileName)
+            print(oldName)
+            print(newFileName)
+            print('-' * 50)
+                
