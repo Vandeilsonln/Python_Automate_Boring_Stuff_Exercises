afe0bdcc3f28b5340c18c1267246e028b61fd61b
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-09-29 22:51:19 -0300

    write_to_reject function created.

--- Chapter-9_Organizing-Files/renamepictures.py
+++ Chapter-9_Organizing-Files/renamepictures.py
@@ -10,6 +10,11 @@ import os, shutil
 from PIL import Image
 
 
+def write_to_reject(rejectedFile):
+    with open('.\\rejected.txt', 'a') as rejected:
+        rejected.write(rejectedFile)
+        rejected.write('\n')
+
 # TODO: Create a list of acceptable extension files and make the code working with all of them.
 def rename_pictures_to_date(rootPath, ext='.jpg'):
     os.chdir(rootPath)
@@ -17,9 +22,7 @@ def rename_pictures_to_date(rootPath, ext='.jpg'):
     for folderName, subfolders, filenames in os.walk(root):
         for file in filenames:
             if not file.endswith(ext):
-                with open('.\\rejected.txt', 'a') as rejected:
-                    rejected.write(file)
-                    rejected.write('\n')
+                write_to_reject(file)
                 continue
             else:
                 oldName = os.path.abspath(folderName) + '\\' + file
@@ -30,8 +33,7 @@ def rename_pictures_to_date(rootPath, ext='.jpg'):
                         newFileName = root + '\\' + (info[36867].replace(':', '_')) + ext
                         # shutil.move(oldName, newFileName)
                     except:
-                        # TODO: Create a function write_to_reject(file) in this case AND for the files with other file extensions as well
-                        pass
+                        write_to_reject(file)
     return 'Done.'
 
 print(rename_pictures_to_date(r'C:\Users\aline\Pictures\Viagens', '.jpg'))
