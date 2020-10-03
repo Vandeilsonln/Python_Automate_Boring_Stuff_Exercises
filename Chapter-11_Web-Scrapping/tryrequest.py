28cad8bd70fbaaba7d78efa923d950a0c5c9cd7b
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-03 00:36:09 -0300

    Add a html to work with tryrequests.py
    By the way, it is the first time I created a HTML file. =)

--- Chapter-11_Web-Scrapping/tryrequest.py
+++ Chapter-11_Web-Scrapping/tryrequest.py
@@ -21,3 +21,19 @@ playFile = open('RomeoAndJuliet.txt', 'wb')
 for chunk in res.iter_content(100000):
     playFile.write(chunk)
 playFile.close()
+
+print('-' * 50)
+
+import bs4
+
+
+res = requests.get('https://nostarch.com/')
+res.raise_for_status()
+noStarchSoup = bs4.BeautifulSoup(res.text)
+print(type(noStarchSoup))
+
+
+exampleFile = open('example.html')
+exampleSoup = bs4.BeautifulSoup(exampleFile)
+
+print(exampleSoup)
