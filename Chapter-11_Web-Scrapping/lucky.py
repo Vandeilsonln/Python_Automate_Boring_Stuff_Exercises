6c2a2306de468a419c25ec7d0eb628841f641c25
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-03 21:39:48 -0300

    Update lucky.py
    Add a condition to make the program search based on your clipboard or through the command line. It is still not working, though.

--- Chapter-11_Web-Scrapping/lucky.py
+++ Chapter-11_Web-Scrapping/lucky.py
@@ -1,20 +1,27 @@
 #! python3
 # lucky.py - Opens several Google search results.
 
-import requests, sys, webbrowser, bs4
+import requests, sys, webbrowser, bs4, pyperclip
 
 
 print('Googling...')    # display text while downloading the Google page.
 
-res = requests.get('https://www.google.com/search?q=' + ''.join(sys.argv[1:]))
-res.raise_for_status()
 
+# It will get arguments either from sys.argv or clipboard
+
+if len(sys.argv) > 1:   # get from command line (sys.argv)
+    res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
+
+else:   # get from the clipboard
+    res = requests.get('https://google.com/search?=' + ' '.join(pyperclip.paste()))
+    print('clipboard', pyperclip.paste())   # checking
 # Retrieve top search result links.
+
+res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text)
 
 # Open a browser tab for each result.
-linkElems = soup.select('.r a')
+linkElems = soup.select('.yuRUbf a')
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
     webbrowser.open('https://google.com' + linkElems[i].get('href'))
-    
