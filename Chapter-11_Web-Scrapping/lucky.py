6f43ba69227b75f7e13162a947d721b93a87e5e8
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-03 21:58:45 -0300

    Update lucky.py
    Tried to change the class on soup.select() method. It opens 5 links, but not the right ones.

--- Chapter-11_Web-Scrapping/lucky.py
+++ Chapter-11_Web-Scrapping/lucky.py
@@ -13,15 +13,15 @@ if len(sys.argv) > 1:   # get from command line (sys.argv)
     res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
 
 else:   # get from the clipboard
-    res = requests.get('https://google.com/search?=' + ' '.join(pyperclip.paste()))
+    res = requests.get('https://google.com/search?=' + '+'.join(pyperclip.paste().split()))
     print('clipboard', pyperclip.paste())   # checking
 # Retrieve top search result links.
 
 res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text, features='html.parser')
 
 # Open a browser tab for each result.
-linkElems = soup.select('.yuRUbf a')
+linkElems = soup.select('a', {'class': 'ra'})
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
     webbrowser.open('https://google.com' + linkElems[i].get('href'))
