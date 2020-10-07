c0a3b467b6cbddb84c1648d5fe6df6facd298d08
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-06 22:39:18 -0300

    Update downloadXkcd.py

--- Chapter-11_Web-Scrapping/downloadXkcd.py
+++ Chapter-11_Web-Scrapping/downloadXkcd.py
@@ -16,13 +16,25 @@ while not url.endswith('#'):
 
     soup = bs4.BeautifulSoup(res.text)
 
-    # TODO: Find the URL of the comic image.
+    # Find the URL of the comic image.
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Could not find comic image.')
+    else:
+        comicUrl = comicElem[0].get('src')
+        # Download the image.
+        print('Downloading image %s...' % (comicUrl))
+        res.requests.get(comicUrl)
+        res.raise_for_status()
 
-    # TODO: Download ther image.
+        # Save the image to the folder.
+        imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+        for chunk in res.iter_content(100000):
+            imageFile.write(chunk)
+        imageFile.close()
 
-    # TODO: Save the image to the folder.
+    # Get the Prev button's url.
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = 'https://xkcd.com' + prevLink.get('href')
 
-    # TODO: Get the Prev button's url.
-
-    pass
 print('Done.')
