42442252afca218fa19711c4129c1db129645f39
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-13 20:51:04 -0300

    Update selenium_v2.py

--- Chapter-11_Web-Scrapping/selenium_v2.py
+++ Chapter-11_Web-Scrapping/selenium_v2.py
@@ -5,20 +5,17 @@ import time
 PATH = r"C:\Program Files (x86)\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
 
-print(type(browser))
+browser.get('https://cursos.alura.com.br//loginForm')
+browser.implicitly_wait(5)
+searchElem = browser.find_element_by_id('login-email')
+searchElem.send_keys('vandernobrel@gmail.com')
 
-browser.get('https://inventwithpython.com')
+passElem = browser.find_element_by_id('password')
+passElem.send_keys('nopasswordhere')
+passElem.submit()
 
-try:
-    elem = browser.find_element_by_class_name('col-sm-12')
-    print('Found %s element with that class name!' % (elem.tag_name))
-except:
-    print('It was not able to find an element with that name.')
-
-linkElem = browser.find_element_by_link_text("See what's new in the second edition.")
-
-print(type(linkElem))
-linkElem.click()    # follows the link
+# nextElem = browser.find_element_by_class_name('btn-login')
+# nextElem.click()
 
 
 time.sleep(3)
