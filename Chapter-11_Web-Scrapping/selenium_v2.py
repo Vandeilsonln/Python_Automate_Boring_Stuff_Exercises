3c1214003bb1dd93c3873a2ca84cbf575a758dd9
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-13 22:24:37 -0300

    Update selenium_v2.py

--- Chapter-11_Web-Scrapping/selenium_v2.py
+++ Chapter-11_Web-Scrapping/selenium_v2.py
@@ -1,25 +1,17 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
 
 
 PATH = r"C:\Program Files (x86)\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
 
-print(type(browser))
-
-browser.get('https://inventwithpython.com')
-
-try:
-    elem = browser.find_element_by_class_name('col-sm-12')
-    print('Found %s element with that class name!' % (elem.tag_name))
-except:
-    print('It was not able to find an element with that name.')
-
-linkElem = browser.find_element_by_link_text("See what's new in the second edition.")
-
-print(type(linkElem))
-linkElem.click()    # follows the link
-
+browser.get('https://nostarch.com')
+htmlElem = browser.find_element_by_tag_name('html')
+time.sleep(1)
+htmlElem.send_keys(Keys.END)    # scrolls to bottom
+time.sleep(1)
+htmlElem.send_keys(Keys.HOME)   # scrolls to top
 
 time.sleep(3)
 browser.close()
