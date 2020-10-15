680ff7c7281b0a77a23ec9a69193434e02d54752
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-14 23:19:28 -0300

    Create imagedownloader.py

---
+++ Chapter-11_Web-Scrapping/imagedownloader.py
@@ -0,0 +1,26 @@
+#! python3
+# imagedownloader.py - Goes to imgur.com website, search  for a few pictures and download them.
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import urllib
+
+PATH = r"C:\Program Files (x86)\chromedriver.exe"
+browser = webdriver.Chrome(PATH)
+
+# Get to the website
+browser.get('https://imgur.com/')
+
+# TODO: Agree with the cookies
+# sleep(3)
+# browser.find_element_by_xpath().click()
+
+# Search something
+seachBar = browser.find_element_by_class_name('Searchbar-textInput')
+seachBar.send_keys('Ireland')
+seachBar.submit()
+
+# identify the images
+
+# download them
