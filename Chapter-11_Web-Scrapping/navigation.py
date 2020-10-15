21d9b92b0c28af84c6aaef1afe801f157c387708
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-14 22:02:50 -0300

    Update navigation.py

--- Chapter-11_Web-Scrapping/navigation.py
+++ Chapter-11_Web-Scrapping/navigation.py
@@ -17,6 +17,5 @@ sleep(1)
 course = browser.find_element_by_link_text('Beginner Python Tutorials')
 course.click()
 
-sleep(2)
-html = browser.find_element_by_tag_name('html')
-html.send_keys(Keys.RETURN)
+sleep(1)
+browser.back()
