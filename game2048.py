47388e8d46eac226658c881ca280467a21bf88e1
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-15 20:02:32 -0300

    Update game2048.py

--- game2048.py
+++ game2048.py
@@ -4,7 +4,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-
+from random import randint
 
 PATH = r"C:\Program Files (x86)\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
@@ -13,8 +13,15 @@ browser = webdriver.Chrome(PATH)
 browser.get('https://play2048.co/')
 
 # Create a list of actions
+actions = ['Keys.UP', 'Keys.RIGHT', 'Keys.DOWN', 'Keys.LEFT']
 
 # Play! (a specific number of times)
+page = browser.find_element_by_tag_name('html')
+for i in range(250):
+    a = randint(0, len(actions)-1)
+    print(a)
+    page.send_keys(actions[a])
+
 
 # Get the score
 
