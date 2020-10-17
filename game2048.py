c3c78b78d132c535f391501deb8945cd8d860514
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-16 23:23:15 -0300

    Update game2048.py

--- game2048.py
+++ game2048.py
@@ -23,13 +23,13 @@ def play_game(actionList, webPage):
 # Create a list of actions
 actions = [Keys.UP, Keys.RIGHT, Keys.DOWN, Keys.LEFT]
             
-# Play! (a specific number of times)
+# Play!)
 page = browser.find_element_by_tag_name('html')
 play_game(actions, page)
 
 # Get the score
 score = browser.find_element_by_class_name('score-container').text
-print(score)
+print('Your score: ', score)
 
 # Write the score in a .txt file
 
@@ -37,11 +37,17 @@ print(score)
 
 # Click to restart the game
 playAgain = input('Would you like to try another shot? (Y/N)')
-
-if playAgain.lower() == 'y':
+while True:
     print("Okay, let's do it!")
+    
     tryAgain = browser.find_element_by_xpath('//a[text()="Try again"]')
     tryAgain.click()
     play_game(actions, page)
-else:
-    print('See you later...')
+
+    score = browser.find_element_by_class_name('score-container').text
+    print('Your score: ', score)
+    playAgain = input('Would you like to try another shot? (Y/N)')
+    if playAgain.lower() != 'y':
+        break
+
+print('Hope to see you soon!')
