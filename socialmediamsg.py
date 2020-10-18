8820fcabc79615569bbc4a029aa6646dacd91b28
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-18 20:40:41 -0300

    Update socialmediamsg.py

--- socialmediamsg.py
+++ socialmediamsg.py
@@ -8,18 +8,20 @@ PATH = r"C:\Program Files (x86)\chromedriver.exe"
 browser = webdriver.Chrome(PATH)
 
 # Go to the social media and log in
-browser.get('https://pt-br.facebook.com/')
+browser.get('https://www.facebook.com')
 
 email = browser.find_element_by_id('email')
 password = browser.find_element_by_id('pass')
 enter = browser.find_element_by_id('u_0_b')
 
-email.send_keys('your_email')
-password.send_keys('your_password')
+email.send_keys('email')
+password.send_keys('pass')
 enter.click()
-sleep(10)
+sleep(5)
 # Access the message's screen
-messageScreen = browser.find_elements_by_css_selector('[aria-label=Messenger]')
-messageScreen[0].click()
-
+messageScreen = browser.find_element_by_css_selector('[aria-label=Messenger]')
+messageScreen.click()
+sleep(5)
+goToMessenger = browser.find_element_by_xpath('//a[text()="Ver tudo no Messenger"]')
+goToMessenger.click()
 # Send a message to a user
