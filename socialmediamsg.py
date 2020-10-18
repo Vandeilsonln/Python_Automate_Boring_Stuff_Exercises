a2c5b3ea5fe3f9b7fa89d5d6f68539124a48e9a9
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-17 22:45:24 -0300

    Update socialmediamsg.py

--- socialmediamsg.py
+++ socialmediamsg.py
@@ -10,6 +10,16 @@ browser = webdriver.Chrome(PATH)
 # Go to the social media and log in
 browser.get('https://pt-br.facebook.com/')
 
+email = browser.find_element_by_id('email')
+password = browser.find_element_by_id('pass')
+enter = browser.find_element_by_id('u_0_b')
+
+email.send_keys('your_email')
+password.send_keys('your_password')
+enter.click()
+sleep(10)
 # Access the message's screen
+messageScreen = browser.find_elements_by_css_selector('[aria-label=Messenger]')
+messageScreen[0].click()
 
 # Send a message to a user
