293711232fb243c35a57665f0e6ca0d677d1c9e8
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-20 17:53:00 -0300

    Update personalnotes.py

--- Chapter-12_Excel/personalnotes.py
+++ Chapter-12_Excel/personalnotes.py
@@ -1,5 +1,7 @@
 import openpyxl
 import os
+import json
+
 
 os.chdir(r'.\Chapter-12_Excel')
 
@@ -21,7 +23,33 @@ for column in mySheet.iter_cols(min_row=1, max_row=4, min_col=1, max_col=5):
 
 print('-'*40)
 
-for column in mySheet.columns:
-    print(column)
+# for column in mySheet.columns:
+#    print(column)
 
 print('-'*40)
+print('-'*40)
+print('-'*40)
+
+for value in mySheet.iter_rows(min_row=1, max_row=1, values_only=True):
+    print(value)
+print('-'*40)
+for value in mySheet.iter_rows(min_row=2, max_row=5, min_col=3, max_col=9, values_only=True):
+    print(value)
+
+print('-'*40)
+print('-'*40)
+print('-'*40)
+
+databaseCompanies = {}
+for i in mySheet.iter_rows(min_col=3, max_col=9, min_row=2, values_only=True):
+    companyName = i[0]
+    companyInfo = {
+        'address': i[1],
+        'phone No': i[4],
+        'e-mail': i[5],
+        'website': i[6]
+    }
+    databaseCompanies[companyName] = companyInfo
+
+# Using JSON here to be able to format the output for displaying later
+print(json.dumps(databaseCompanies))
