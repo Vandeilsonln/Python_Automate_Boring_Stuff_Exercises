d35b93fbb29ba7a8a611412ba4997c585cabde21
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-20 10:16:20 -0300

    not following the book
    No longer using the book for the notes. Only for the projects.

--- Chapter-12_Excel/personalnotes.py
+++ Chapter-12_Excel/personalnotes.py
@@ -3,27 +3,10 @@ import os
 
 os.chdir(r'.\Chapter-12_Excel')
 
-wb = openpyxl.load_workbook('example.xlsx')
+myWorkBook = openpyxl.Workbook()
+mySheet = myWorkBook.active
 
-sheet = wb.get_sheet_by_name('Planilha1')
-print(sheet['A1'])
+mySheet['A1'] = 'Hello'
+mySheet['A2'] = 'world!'
 
-print(sheet['A1'].value)
-
-c = sheet['B1']
-print(c.value)
-
-string = 'Row ' + str(c.row) + ', Column ' + str(c.column) + ' is ' + str(c.value)
-
-print(string)
-
-print(' Cell ' + str(c.coordinate) + ' is ' + str(c.value))
-
-print(sheet['C1'].value)
-
-print(sheet.cell(row=1, column=2))
-
-for i in range(1, 8, 2):
-    print(i, sheet.cell(row=i, column=2).value)
-
-print(sheet.dimensions.split(':'))
+myWorkBook.save(filename='hello_world.xlsx')
