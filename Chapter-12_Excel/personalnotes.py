7848a11c87a00b1a477afc117e07b864df3554b6
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-20 11:25:19 -0300

    import data from excel

--- Chapter-12_Excel/personalnotes.py
+++ Chapter-12_Excel/personalnotes.py
@@ -5,8 +5,19 @@ os.chdir(r'.\Chapter-12_Excel')
 
 myWorkbook = openpyxl.load_workbook('example.xlsx')
 
-print(myWorkbook.sheetnames)
-
 mySheet = myWorkbook.active
-print(mySheet)
-print(mySheet.title)
+
+print(mySheet['A1:C2'])
+
+print('-'*40)
+
+for row in mySheet.iter_rows(min_row=1, max_row=4, min_col=1, max_col=5):
+    print(row)
+
+print('-'*40)
+
+for column in mySheet.iter_cols(min_row=1, max_row=4, min_col=1, max_col=5):
+    print(column)
+
+print('-'*40)
+
