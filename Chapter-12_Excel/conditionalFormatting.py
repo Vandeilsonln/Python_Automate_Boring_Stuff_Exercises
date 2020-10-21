24caea0dea30a8922314b0a81c78328f037067e7
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-21 18:43:00 -0300

    Update conditionalFormatting.py

--- Chapter-12_Excel/conditionalFormatting.py
+++ Chapter-12_Excel/conditionalFormatting.py
@@ -1,8 +1,14 @@
 from openpyxl import load_workbook
-from openpyxl.utils import FORMULAE
+from openpyxl.styles import PatternFill, colors
+from openpyxl.styles.differential import DifferentialStyle
+from openpyxl.formatting.rule import Rule
 from os import chdir
 
 
 chdir(r'.\Chapter-12_Excel')
 
 workbook = load_workbook(filename='reviews-sample.xlsx')
+
+sheet = workbook.active
+
+# conditional formatting
