f749751762069dbcc35737418026bef4f3058eee
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-21 18:27:08 -0300

    update

--- Chapter-12_Excel/personalnotes2.py
+++ Chapter-12_Excel/personalnotes2.py
@@ -1,4 +1,5 @@
 from openpyxl import load_workbook
+from openpyxl.utils import FORMULAE
 from os import chdir
 
 
@@ -12,60 +13,10 @@ workbook = load_workbook(filename='example.xlsx')
 
 sheet = workbook.active
 
-# cell = sheet['A1']
-# cell.value = 'Hey'
-# print(cell.value)
-
-# print_rows(sheet)
-
-# sheet['B10'] = 'test'
-# print_rows(sheet)
-
-# # Insert a column BEFORE the existing column 1 ('A')
-# sheet.insert_cols(idx=1)
-# print_rows(sheet)
-
-# # Insert 5 columns Between column 2 ('B') and 3 ('C')
-# sheet.insert_cols(idx=3, amount=5)
-# print_rows(sheet)
-
-# # delete th created columns
-# sheet.delete_cols(idx=3, amount=5)
-# sheet.delete_cols(idx=1)
-# print_rows(sheet)
-
-# sheet.delete_rows(idx=3, amount=7)
-# print_rows(sheet)
-
-# print(workbook.sheetnames)
-
-# sheet.title = 'Python_Test'
-
-# print(workbook.sheetnames)
-
-# testSheet = workbook.create_sheet('Testing_openpyxl')
-
-# print(workbook.sheetnames)
-
-# idxSheet = workbook.create_sheet('Indexed', 0)
-
-# print(workbook.sheetnames)
-
-# workbook.remove(idxSheet)
-# workbook.remove(testSheet)
-
-# print(workbook.sheetnames)
-
-# workbook.copy_worksheet(sheet)
-
-# print(workbook.sheetnames)
-# workbook.save('hello_world.xlsx')
-
-# sheet.freeze_panes = 'C2'
-
-# Check the used spreadsheet space using the atribute 'dimensions'
-
-print(sheet.dimensions)
-
 sheet.auto_filter.ref= sheet.dimensions
 workbook.save(filename='example.xlsx')
+
+# column test is "G"
+sheet['G502'] = '=COUNTA(G2:G501)'
+workbook.save(filename='example.xlsx')
+
