93f0a283821cb4c6f463de06fe52426da69e5b8a
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-22 10:48:30 -0300

    update

--- Chapter-12_Excel/conditionalFormatting.py
+++ Chapter-12_Excel/conditionalFormatting.py
@@ -8,7 +8,14 @@ from os import chdir
 chdir(r'.\Chapter-12_Excel')
 
 workbook = load_workbook(filename='reviews-sample.xlsx')
-
 sheet = workbook.active
 
-# conditional formatting
+# conditional formatting cofigurations
+red_backgroung = PatternFill(bgColor=colors.BLUE)
+diff_style = DifferentialStyle(fill=red_backgroung)
+rule = Rule(type='expression', dxf=diff_style)
+rule.formula = ['$H1<3']
+
+sheet.conditional_formatting.add(sheet.dimensions, rule)
+
+workbook.save('reviews-sample.xlsx')
