a3ad26871e0bd61fac18cb84c64a39c78b6f0f2b
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-22 11:18:44 -0300

    Color Scale min and max

--- Chapter-12_Excel/conditionalFormatting.py
+++ Chapter-12_Excel/conditionalFormatting.py
@@ -1,5 +1,6 @@
 from openpyxl import load_workbook
-from openpyxl.styles import PatternFill, colors
+from openpyxl.styles import PatternFill
+from openpyxl.styles.colors import Color
 from openpyxl.styles.differential import DifferentialStyle
 from openpyxl.formatting.rule import Rule
 from openpyxl.formatting.rule import ColorScaleRule
@@ -11,12 +12,9 @@ chdir(r'.\Chapter-12_Excel')
 workbook = load_workbook(filename='reviews-sample.xlsx')
 sheet = workbook.active
 
-# conditional formatting cofigurations
-red_backgroung = PatternFill(bgColor=colors.Color(indexed=(10)))
-diff_style = DifferentialStyle(fill=red_backgroung)
-rule = Rule(type='expression', dxf=diff_style)
-rule.formula = ['$H1<3']
+# conditional formatting color scale
+color_scale_rule = ColorScaleRule(start_type='min', start_color=Color(indexed=37), end_type='max', end_color=Color(indexed=58))
 
-sheet.conditional_formatting.add(sheet.dimensions, rule)
+sheet.conditional_formatting.add('H2:H100', color_scale_rule)
 
 workbook.save('reviews-sample.xlsx')
