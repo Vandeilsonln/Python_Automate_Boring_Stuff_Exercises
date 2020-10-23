b9e777763ef3e6c99587b23219d714ffd286fa15
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-22 21:47:35 -0300

    data bar

--- Chapter-12_Excel/conditionalFormatting.py
+++ Chapter-12_Excel/conditionalFormatting.py
@@ -1,5 +1,6 @@
 from openpyxl import load_workbook
-from openpyxl.formatting.rule import IconSetRule
+from openpyxl.formatting.rule import DataBarRule
+from openpyxl.styles.colors import Color
 from os import chdir
 
 chdir(r'.\Chapter-12_Excel')
@@ -7,7 +8,7 @@ chdir(r'.\Chapter-12_Excel')
 workbook = load_workbook(filename='reviews-sample.xlsx')
 sheet = workbook.active
 
-icon_rule = IconSetRule('5Arrows', 'num',[1, 2, 3, 4, 5])
-sheet.conditional_formatting.add('H2:H100', icon_rule)
+data_bar_rule = DataBarRule(start_type='num', start_value=1,end_type='num', end_value=5, color=Color(indexed=17))
+sheet.conditional_formatting.add('H2:H100', data_bar_rule)
 
 workbook.save('reviews-sample.xlsx')
