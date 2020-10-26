d4b2438e5e794182fac94791ad93f51f57aa3b0b
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-25 22:41:49 -0300

    Create multiplication.py

---
+++ Chapter-11_Web-Scrapping/multiplication.py
@@ -0,0 +1,18 @@
+#! python3
+# multiplication.py - Creates a excel spreadsheet with a 
+# multiplication table inside
+
+
+import openpyxl
+from os import chdir
+
+# set up directory
+chdir(r'Chapter-12_Excel')
+
+# create a workbook and a sheet
+
+# create a function to build the table
+
+# send it to the worksheet
+
+# save
