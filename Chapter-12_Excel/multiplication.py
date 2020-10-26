5788d94d24812772c35e914e193e256d9babdb87
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-10-25 22:57:57 -0300

    Update multiplication.py

--- Chapter-12_Excel/multiplication.py
+++ Chapter-12_Excel/multiplication.py
@@ -10,9 +10,28 @@ from os import chdir
 chdir(r'Chapter-12_Excel')
 
 # create a workbook and a sheet
+workbook = openpyxl.Workbook()
+sheet = workbook.active
 
 # create a function to build the table
+def createTable(size):
+    # size: integer
+    # return: list
+    myTable = []
+
+    for colnum in range(1, size+1):
+        # myTable will have a list inside for each row
+        dataTable = []
+        for rownum in range(1, size+1):
+            result = colnum * rownum
+            dataTable.append(result)
+        myTable.append(dataTable)
+
+    return myTable
+# create headers
 
 # send it to the worksheet
 
 # save
+
+print(createTable(5))
