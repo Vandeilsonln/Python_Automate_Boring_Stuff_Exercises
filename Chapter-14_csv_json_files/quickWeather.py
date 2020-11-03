9c14a5634e8f8e00929cfe7835574cb6ca8a567b
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-02 22:52:56 -0300

    Update quickWeather.py

--- Chapter-14_csv_json_files/quickWeather.py
+++ Chapter-14_csv_json_files/quickWeather.py
@@ -2,19 +2,22 @@
 # quickWeather.py - Prints the weather for a location frmo the command line.
 
 
-import json, requests, sys
+import json, requests, sys, pprint
 
 
 # Compute location from command line arguments.
-if len(sys.argv) < 2:
-    print('Usage: quickWeather.py location')
-    sys.exit()
-location = ' '.join(sys.argv[1:])
-
-#TODO: Download the JSON data from OpenWeatherMap.org's API.
-url = f'http:api.openweathermap.org/data/2.5/forecast/daily?q={location}&cnt=3'
+#if len(sys.argv) < 2:
+ #   print('Usage: quickWeather.py location')
+  #  sys.exit()
+location = 'Sao Paulo, Brazil' #' '.join(sys.argv[1:])
+key = ''
+# Download the JSON data from OpenWeatherMap.org's API.
+url = f'https://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&lang=pt_br&appid={key}'
 
 response = requests.get(url)
 response.raise_for_status()
 
-#TODO: Load JSON data into a Python variable.
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+
+pprint.pprint(weatherData)
