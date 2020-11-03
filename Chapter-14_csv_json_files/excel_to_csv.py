08888a8317675e1303940b3e040906d4692d472b
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-03 16:06:33 -0300

    Update excel_to_csv.py

--- Chapter-14_csv_json_files/excel_to_csv.py
+++ Chapter-14_csv_json_files/excel_to_csv.py
@@ -19,19 +19,23 @@ for excelFile in os.listdir(path):
         sheet = wb.get_sheet_by_name(sheetName)
 
         # Create the CSV filename from the Excel filename and sheet title.
-        excelName = excelFile.strip('.xlsx')
+        excelName = excelFile.split('.')[0]
         sheetName = sheet.title
         csvName = excelName + '_' + sheetName + '.csv'
         print(csvName)
-        
+
         # Create the csv.writer object for this CSV file.
-        csvFile = csv.writer(csvName)
+        csvFileObj = open(csvName, 'w')
+        csvFile = csv.writer(csvFileObj)
 
         # Loop through every row in the sheet.
-        for rowNum in range(1, sheet.get_highest_row() + 1):
+        for rowNum in range(1, sheet.max_row + 1):
             rowData = []    # Append each cell to this list
             # Loop through each cell in the row.
-            for colNum in range(1, sheet.get_highest_column() + 1):
+            for colNum in range(1, sheet.max_column + 1):
                 # Append each cell's data to rowData.
-                pass
+                rowData.append(colNum)
             # Write the rowData list to the CSV file.
+            csvFile.writerow(rowData)
+        
+        csvFileObj.close()
