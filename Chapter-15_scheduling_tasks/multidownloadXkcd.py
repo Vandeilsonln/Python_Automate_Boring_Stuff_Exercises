1addefe0475899071fa9a59dba74aa6c16b4b2b6
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-04 19:48:00 -0300

    Create multidownloadXkcd.py

---
+++ Chapter-15_scheduling_tasks/multidownloadXkcd.py
@@ -0,0 +1,26 @@
+#! python3
+# multidownloadXkcd.py - Downloads XKCD comics using multiple threads.
+
+import requests, os, bs4, threading
+os.chdir(r'.\Chapter-15_scheduling_tasks')
+os.makedirs('xkcd', exist_ok=True)
+
+def downloadXkcd(startComic, endComic):
+    for urlNumber in range(startComic, endComic):
+        # Download the page
+        print(f'Downloading page http://xkcd.com/{urlNumber}')
+
+        res = requests.get(f'https://xkcd.com/{urlNumber}')
+
+        res.raise_for_status()
+
+        soup = bs4.BeautifulSoup(res.text)
+
+        # Find the url of the comic image.
+
+        comicElem = soup.select('#comic img')
+        if comicElem == []:
+            print('Could not find comic image')
+        else:
+            comicUrl = comicElem[0].get('src')
+            # Download the image
