7f8645fd1004e142b59c999c0acd1259161599ca
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-03 22:49:26 -0300

    Update personalnotes.py

--- Chapter-15_scheduling_tasks/personalnotes.py
+++ Chapter-15_scheduling_tasks/personalnotes.py
@@ -1,17 +1,8 @@
 import time
+import datetime
 
 
-def calcProd():
-    # Calculate the product of the first 100,000 numbers.
-    product = 1
-    for i in range(1, 40000):
-        product *= i
-    return product
-
-
-startTime = time.time()
-prod = calcProd()
-endTime = time.time()
-
-print(f'The result is {len(str(prod))} digits long.')
-print(f'It took {endTime - startTime} seconds to calculate.')
+dt = datetime.datetime.now()
+print(dt)
+thousandDays = datetime.timedelta(days=1000)
+print(dt + thousandDays)
