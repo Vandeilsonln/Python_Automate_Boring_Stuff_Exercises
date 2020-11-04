89cd3e90765c9b86b26f82e39fba7f5b5eb1c433
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-04 19:00:57 -0300

    Update multiThreading.py

--- Chapter-15_scheduling_tasks/multiThreading.py
+++ Chapter-15_scheduling_tasks/multiThreading.py
@@ -12,3 +12,9 @@ threadObj = threading.Thread(target=takeANap)
 threadObj.start()
 
 print('End of Program.')
+
+print('-' * 40)
+
+threadObj2 = threading.Thread(target=print, args=['Cats', 'Dogs', 'Frogs'], kwargs={'sep': ' & '})
+
+threadObj2.start()
