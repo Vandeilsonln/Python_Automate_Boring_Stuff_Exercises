d2b1cccce94111807078ed64cd0e22fe3519a5fc
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-11-05 00:02:17 -0300

    Update personalnotes.py

--- Chapter-15_scheduling_tasks/personalnotes.py
+++ Chapter-15_scheduling_tasks/personalnotes.py
@@ -1,10 +1,2 @@
-import time
-import datetime
-
-
-oct21st = datetime.datetime(2015, 10, 21, 16, 29, 0)
-print(oct21st.strftime(r'%Y/%m/%d %H:%M:%S'))
-
-print(oct21st.strftime(r'%I:%M:%p'))
-
-print(oct21st.strftime(r'%yth of %B'))
+import subprocess
+subprocess.Popen('C:\\Windows\\System32\\calc.exe')
